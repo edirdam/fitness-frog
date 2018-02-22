0e2d8bd5a66c58ced55c8adfed06cd37d05a3fb4
Enrique Máñez Cortinas <edirdam@gmail.com>
2018-02-22 12:59:47 +0100

    Added confirmation messages

--- src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
+++ src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -58,6 +58,7 @@ namespace Treehouse.FitnessFrog.Controllers
             if (ModelState.IsValid)
             {
                 _entriesRepository.AddEntry(entry);
+                TempData["Message"] = "Your entry was successfully added!";
                 return RedirectToAction("Index");
 
             }
@@ -91,6 +92,8 @@ namespace Treehouse.FitnessFrog.Controllers
             if (ModelState.IsValid)
             {
                 _entriesRepository.UpdateEntry(entry);
+
+                TempData["Message"] = "Your entry was successfully updated!";
                 return RedirectToAction("Index");
             }
 
@@ -121,7 +124,7 @@ namespace Treehouse.FitnessFrog.Controllers
         public ActionResult Delete(int id)
         {
             _entriesRepository.DeleteEntry(id);
-
+            TempData["Message"] = "Your entry was successfully deleted!";
             return RedirectToAction("Index");
         }
 
